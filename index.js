908679671e9687461ef1689195f3ba39fbdaabde
Mubashir Kabir <97.m.kabir@gmail.com>
2022-10-26 00:08:30 +0600

    all courses api created

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const courses = require("./data/courses.json");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("test server is running...");
+});
+app.get("/courses", (req, res) => {
+  res.send(courses);
 });
 
 app.listen(port, () => {
